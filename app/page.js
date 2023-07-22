7f604f9b8de5b61164c610c3fa7b6f2ab2153520
mtsamis2 <mtsamis@gmail.com>
2023-07-22 16:28:30 -0400

    Layout fix 2

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import styles from './page.module.css'
 import { useState } from 'react';
-import Layout from './layout';
 
 export default function Home() {
   const EXAMPLE_DATA_JSON = [
@@ -110,72 +109,67 @@ export default function Home() {
 
   return (
     <div>
-      <Layout
-        title={'Json Prettify/Minify Tool - PrettyPrintJson.com'}
-        description={'Easily format and beautify your JSON code with this online JSON editor. Features include JSON validation, minify, upload, copy, and download. Perfect for developers and anyone working with JSON data.'}
-      >
-        <h2 className={styles.toolHeader}>JSON Prettify/Minify Tool</h2>
-        <div className={styles.container}>
-          <div className={styles.left}>
-            <h3 className={styles.center}>Input</h3>
-            <button onClick={insertSampleJSON}>Sample JSON</button>
-            <button onClick={clear}>Clear</button>
-            <textarea value={input} onChange={handleChange} />
-            <input className={styles.fileInput} type="file" onChange={handleFileChange} />
-          </div>
-          <div className={styles.center}>
-              <button onClick={prettyPrint}> Prettify</button>
-              <button onClick={minify}>Minify</button>
-          </div>
-          <div className={styles.right}>
-            <h3 className={styles.center}>Output</h3>
-            <button onClick={copyToClipboard}>Copy</button>
-            <button onClick={download}>Download</button>
-            <textarea value={output} readOnly />
-          </div>
+      <h1 className={styles.toolHeader}>JSON Prettify/Minify Tool</h1>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h3 className={styles.center}>Input</h3>
+          <button onClick={insertSampleJSON}>Sample JSON</button>
+          <button onClick={clear}>Clear</button>
+          <textarea value={input} onChange={handleChange} />
+          <input className={styles.fileInput} type="file" onChange={handleFileChange} />
         </div>
-        <div>
-          <h2>Instructions for using JSON Prettify/Minify</h2>
-          <ol>
-            <li>Copy/paste your JSON data into the "Input" text area or upload a JSON file using the "Choose File" button.</li>
-            <li>You can format and beautify the data by clicking on the "Prettify" button. This will automatically indent the JSON data and make it more readable.</li>
-            <li>If you prefer to have the data in a more compact format to conserve space, you can click on the "Minify" button to remove any extra white spaces from the JSON data.</li>
-            <li>The prettified/minified JSON data will appear in the "Output" text area. You can copy the output data to your clipboard by clicking on the "Copy" button.</li>
-            <li>Click "Download" to download a .JSON file with your output.</li>
-            <li>Click "Clear" to clear all input.</li>
-            <li>Click "Sample JSON" if you need to generate dummy JSON data.</li>
-          </ol>
-          <p>That's all there is to it! With this JSON prettify/minify editor tool, you can easily format and beautify your JSON data, making it easier to read and understand.</p>
+        <div className={styles.center}>
+            <button onClick={prettyPrint}> Prettify</button>
+            <button onClick={minify}>Minify</button>
         </div>
-        <div>
-          <h2>What is JSON?</h2>
-          <p>JSON (JavaScript Object Notation) is a simple format for exchanging data. It can be easily read, written and analyzed by both us and machines. It's based on a subset of the JavaScript programming language and is mostly used to transfer data between a server and web application. JSON is a completely language-independent textual format, but uses conventions familiar to programmers from the C family of languages such as C, C++, C#, Java, JavaScript, Perl, and Python. JSON is often used as a simple data exchange format between different systems, making it easy to read and write.</p>
-          <p>An example of JSON data is a list of objects, each representing a person, with properties such as name, age, and address:</p>
-          <pre>
-            <code>
-              {JSON.stringify(EXAMPLE_DATA_JSON, null, 2)}
-            </code>
-          </pre>
-          <p>In the above example, we have an array of objects where each property is a key-value pair. Each object above contains a person's name, age and address.</p>
-          <h2>What's the Difference Between JSON and JSONP and Which Do I Use?</h2>
-          <p>JSON and JSONP are two data formats commonly used in web development to exchange data between the client and server. Here's the difference between JSON and JSONP and when you should use each one.</p>
-          <p>JSON is used to represent data in a structured format and is commonly used in APIs to send data between the client and server. The content type for JSON data should be set to <b>"application/json"</b>.</p>
-          <p>JSONP (JSON with Padding) is a technique used to overcome the same-origin policy limitation placed by browsers. The same-origin policy prevents a website from making requests to a different domain from the website's. JSONP works by wrapping the JSON data in a callback function and passing that JSON data as an argument to that function like this:</p>
-          <pre>
-            <code>
-              {`myFunctionCall({"Name": "John", "Sport": "Baseball"});`}
-            </code>
-          </pre>
-          <p>The content type of JSONP data should be set to <b>"application/javascript"</b>.</p>
-          <p>Therefore, if you need to exchange data between the client and server within the same domain, use JSON. Use JSONP when you need to exchange data between the client and server in different domains. Both JSON and JSONP are useful data formats with different purposes and use cases.</p>
-          <p>Here are some other great resources explaining JSON format in more detail:</p>
-          <ul>
-            <li><a title="JSON.org" href="https://json.org/">JSON.org</a></li>
-            <li><a title="w3schools.com's JSON Tutorial" href="https://www.w3schools.com/js/js_json_intro.asp">JSON Tutorial from w3schools.com</a></li>
-            <li><a title="Wikipedia link to JSON" href="https://en.wikipedia.org/wiki/JSON">JSON Explained on Wikipedia</a></li>
-          </ul>
+        <div className={styles.right}>
+          <h3 className={styles.center}>Output</h3>
+          <button onClick={copyToClipboard}>Copy</button>
+          <button onClick={download}>Download</button>
+          <textarea value={output} readOnly />
         </div>
-      </Layout>
+      </div>
+      <div>
+        <h2>Instructions for using JSON Prettify/Minify</h2>
+        <ol>
+          <li>Copy/paste your JSON data into the "Input" text area or upload a JSON file using the "Choose File" button.</li>
+          <li>You can format and beautify the data by clicking on the "Prettify" button. This will automatically indent the JSON data and make it more readable.</li>
+          <li>If you prefer to have the data in a more compact format to conserve space, you can click on the "Minify" button to remove any extra white spaces from the JSON data.</li>
+          <li>The prettified/minified JSON data will appear in the "Output" text area. You can copy the output data to your clipboard by clicking on the "Copy" button.</li>
+          <li>Click "Download" to download a .JSON file with your output.</li>
+          <li>Click "Clear" to clear all input.</li>
+          <li>Click "Sample JSON" if you need to generate dummy JSON data.</li>
+        </ol>
+        <p>That's all there is to it! With this JSON prettify/minify editor tool, you can easily format and beautify your JSON data, making it easier to read and understand.</p>
+      </div>
+      <div>
+        <h2>What is JSON?</h2>
+        <p>JSON (JavaScript Object Notation) is a simple format for exchanging data. It can be easily read, written and analyzed by both us and machines. It's based on a subset of the JavaScript programming language and is mostly used to transfer data between a server and web application. JSON is a completely language-independent textual format, but uses conventions familiar to programmers from the C family of languages such as C, C++, C#, Java, JavaScript, Perl, and Python. JSON is often used as a simple data exchange format between different systems, making it easy to read and write.</p>
+        <p>An example of JSON data is a list of objects, each representing a person, with properties such as name, age, and address:</p>
+        <pre>
+          <code>
+            {JSON.stringify(EXAMPLE_DATA_JSON, null, 2)}
+          </code>
+        </pre>
+        <p>In the above example, we have an array of objects where each property is a key-value pair. Each object above contains a person's name, age and address.</p>
+        <h2>What's the Difference Between JSON and JSONP and Which Do I Use?</h2>
+        <p>JSON and JSONP are two data formats commonly used in web development to exchange data between the client and server. Here's the difference between JSON and JSONP and when you should use each one.</p>
+        <p>JSON is used to represent data in a structured format and is commonly used in APIs to send data between the client and server. The content type for JSON data should be set to <b>"application/json"</b>.</p>
+        <p>JSONP (JSON with Padding) is a technique used to overcome the same-origin policy limitation placed by browsers. The same-origin policy prevents a website from making requests to a different domain from the website's. JSONP works by wrapping the JSON data in a callback function and passing that JSON data as an argument to that function like this:</p>
+        <pre>
+          <code>
+            {`myFunctionCall({"Name": "John", "Sport": "Baseball"});`}
+          </code>
+        </pre>
+        <p>The content type of JSONP data should be set to <b>"application/javascript"</b>.</p>
+        <p>Therefore, if you need to exchange data between the client and server within the same domain, use JSON. Use JSONP when you need to exchange data between the client and server in different domains. Both JSON and JSONP are useful data formats with different purposes and use cases.</p>
+        <p>Here are some other great resources explaining JSON format in more detail:</p>
+        <ul>
+          <li><a title="JSON.org" href="https://json.org/">JSON.org</a></li>
+          <li><a title="w3schools.com's JSON Tutorial" href="https://www.w3schools.com/js/js_json_intro.asp">JSON Tutorial from w3schools.com</a></li>
+          <li><a title="Wikipedia link to JSON" href="https://en.wikipedia.org/wiki/JSON">JSON Explained on Wikipedia</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
